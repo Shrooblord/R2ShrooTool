3a1a91d3527482630985c23e89903cfacfaeb85e
Jango Course <jango.course@gmail.com>
2020-02-27 15:45:12 +0100

    - Retouched Magic Fist behaviour, and made Rules compliant with the original RaymapGame template for Alw_Projectile_Rayman_Model assets.

--- Assets/Scripts/Shrooblord/perso/projectiles/Models/Alw_Projectile_Rayman_Model/RayMagicFist.cs
+++ Assets/Scripts/Shrooblord/perso/projectiles/Models/Alw_Projectile_Rayman_Model/RayMagicFist.cs
@@ -12,33 +12,34 @@ namespace RaymapGame.Rayman2.Persos {
     ///     
     public partial class RayMagicFist : Alw_Projectile_Rayman_Model {
         protected override void OnStart() {
-            //pos = rayman.pos;
             rot = rayman.rot;
             SetRule("Charging");
         }
 
         #region Rules
         protected void Rule_Charging() {
-            scale += 0.02f;
+            if (scale < 2.0f)
+                scale += 0.02f;
         }
 
         Timer StartDieTimer = new Timer();
         protected void Rule_Release() {
-            moveSpeed = 23;
-            SetFriction(100, 100);
             StartDieTimer.Start(1f, () => SetRule("Weakening"), false);
-            SetRule("Shoot");
+            SetRule("ShootProjectile");
         }
 
         Timer DieTimer = new Timer();
-        protected void Rule_Shoot() {
-            if (newRule) DieTimer.Start(2f, () => SetRule("Die"), false);
-            NavForwards();
+        protected void Rule_ShootProjectile() {
+            Rule_Shoot();
+            if (newRule) {
+                DieTimer.Start(2f, () => SetRule("Die"), false);
+                pos = shooter.pos + Vector3.up + shooter.right;
+            }
         }
 
         protected void Rule_Weakening() {
             if (newRule) anim.Set(1);
-            NavForwards();
+            //NavForwards();
         }
 
         protected void Rule_Die() {
