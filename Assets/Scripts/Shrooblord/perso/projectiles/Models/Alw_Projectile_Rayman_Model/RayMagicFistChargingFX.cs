985d6d80b321aac50cc59f09b3c22d531715c782
Jango Course <jango.course@gmail.com>
2020-02-28 19:21:26 +0100

    - Removed scaling and position setting for the Magic Fist charging FX. This gives it the proper look as seen in R2. - Removed /bin/ folder and moved it up to R2RRotR.

--- Assets/Scripts/Shrooblord/perso/projectiles/Models/Alw_Projectile_Rayman_Model/RayMagicFistChargingFX.cs
+++ Assets/Scripts/Shrooblord/perso/projectiles/Models/Alw_Projectile_Rayman_Model/RayMagicFistChargingFX.cs
@@ -11,7 +11,6 @@ namespace RaymapGame.Rayman2.Persos {
     /// </summary>
     ///     
     public partial class RayMagicFistChargingFX : Alw_Projectile_Rayman_Model {
-        float waitTimer;
         protected override void OnStart() {
             SetRule("Init");
         }
@@ -29,19 +28,11 @@ namespace RaymapGame.Rayman2.Persos {
             if (newRule)
                 Timer.StartNew(0.5f, Remove);   //these particle fx are short-lived
 
-            rot = shooter.rot;
-            //pos = shooter.pos + Vector3.up * 1.2f + shooter.right * 0.8f - shooter.forward * 0.3f;
+            //rot = shooter.rot;
 
             if (shooter == rayman) {
-                pos = rayman.perso.channelObjects[2].transform.position + rayman.forward * 0.12f + rayman.right * 0.28f + rayman.upward * 0.15f;
+                //pos = rayman.perso.channelObjects[2].transform.position + rayman.forward * 0.12f + rayman.right * 0.28f + rayman.upward * 0.15f;
             }
-
-            if (scale < 1.10f && waitTimer > 0.1f)
-                scale += 0.5f * dt;
-            if (scale > 1.10f)
-                scale = 1.10f;
-
-            waitTimer += dt;
         }
         #endregion
     }
